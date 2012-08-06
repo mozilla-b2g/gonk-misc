4bee3d75415a6298aef301c405485eef69439828
Michael Wu <mwu@mozilla.com>
2012-08-06 18:54:34 -0400

    Add b2g.sh script to start b2g
    This prevents us from passing LD_PRELOAD to other programs.

--- Android.mk
+++ Android.mk
@@ -38,6 +38,13 @@ LOCAL_SRC_FILES    := init.b2g.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := b2g.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := DATA
+LOCAL_SRC_FILES    := b2g.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := httpd.conf
---
+++ b2g.sh
@@ -0,0 +1,6 @@
+#!/system/bin/sh
+export TMPDIR=/data/local/tmp
+export LD_LIBRARY_PATH=/vendor/lib:/system/lib:/system/b2g
+export LD_PRELOAD=/system/b2g/libmozglue.so
+export GRE_HOME=/system/b2g
+exec /system/b2g/b2g
