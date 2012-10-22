5bb8f9eea0f68f8134b0d62a07fd3e317e139d0b
Michael Wu <mwu@mozilla.com>
2012-10-22 16:39:31 -0400

    Put b2g.mk here instead of platform_build

---
+++ b2g.mk
@@ -0,0 +1,37 @@
+PRODUCT_NAME := b2g
+PRODUCT_BRAND := generic
+PRODUCT_DEVICE := generic
+
+TARGET_PROVIDES_INIT_RC := true
+CONFIG_ESD := no
+HTTP := android
+
+PRODUCT_PACKAGES := \
+	b2g.sh \
+	fakeperm \
+	gaia \
+	gecko \
+	init.rc \
+	init.b2g.rc \
+	rilproxy \
+	sources.xml \
+	OpenSans-BoldItalic.ttf \
+	OpenSans-Bold.ttf \
+	OpenSans-ExtraBoldItalic.ttf \
+	OpenSans-ExtraBold.ttf \
+	OpenSans-Italic.ttf \
+	OpenSans-LightItalic.ttf \
+	OpenSans-Light.ttf \
+	OpenSans-Regular.ttf \
+	OpenSans-SemiboldItalic.ttf \
+	OpenSans-Semibold.ttf \
+	MozTT-Light.ttf \
+	MozTT-Regular.ttf \
+	MozTT-Medium.ttf \
+	MozTT-Bold.ttf \
+	$(NULL)
+
+ifeq ($(ENABLE_LIBRECOVERY),true)
+PRODUCT_PACKAGES += \
+  librecovery
+endif
