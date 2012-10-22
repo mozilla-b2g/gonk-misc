db0c751715f4696515735eb1e0dc5df7a40eb81d
Michael Wu <mwu@mozilla.com>
2012-10-22 17:29:42 -0400

    cleanup b2g.mk a bit

--- b2g.mk
+++ b2g.mk
@@ -1,12 +1,8 @@
-PRODUCT_NAME := b2g
-PRODUCT_BRAND := generic
-PRODUCT_DEVICE := generic
-
 TARGET_PROVIDES_INIT_RC := true
 CONFIG_ESD := no
 HTTP := android
 
-PRODUCT_PACKAGES := \
+PRODUCT_PACKAGES += \
 	b2g.sh \
 	fakeperm \
 	gaia \
