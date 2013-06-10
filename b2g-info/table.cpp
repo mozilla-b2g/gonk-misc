ceecb60c01a433b9c6fcf8b8710a43b1d781e889
Justin Lebar <justin.lebar@gmail.com>
2013-06-10 10:55:38 -0400

    b2g-info: Fix copyright year.

--- b2g-info/table.cpp
+++ b2g-info/table.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Mozilla Foundation
+ * Copyright (C) 2013 Mozilla Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
